1c22bf0d4c25a74920dba202821603abf062eed9
GameStuffOffical <66188005+GameStuffOffical@users.noreply.github.com>
2022-01-25 19:35:51 -0600

    Update arc.js

--- scripts/blocks/arc.js
+++ scripts/blocks/arc.js
@@ -2,4 +2,4 @@ function rN(min, max) {
   return Math.floor(Math.random() * (max - min) ) + min;
 };
 
-Blocks.arc.shootType.lightningLength = rN(10, 30);
+Blocks.arc.shootType.lightningLength = rN(5, 60);
