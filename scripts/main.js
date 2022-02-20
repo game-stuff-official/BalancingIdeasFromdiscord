338b3e6187d449e55598fbeae9741a59859399b4
GameStuffOffical <66188005+GameStuffOffical@users.noreply.github.com>
2022-02-19 19:17:31 -0600

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -8,5 +8,6 @@ UnitTypes.vela.boostMultiplier *= 3;
 UnitTypes.vela.speed *= 4;
 
 // Atrax
-UnitTypes.atrax.legLength *= 2;
-UnitTypes.atrax.legCount *= 2;
+UnitTypes.atrax.legLength *= 4;
+UnitTypes.atrax.legCount *= 4;
+UnitTypes.atrax.speed *= 4;
