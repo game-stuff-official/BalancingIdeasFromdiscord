57274b15c659212978f9dbad1c71e5afd97b2cb5
GameStuffOffical <66188005+GameStuffOffical@users.noreply.github.com>
2022-02-19 18:50:33 -0600

    Update vela.js

--- scripts/units/vela.js
+++ scripts/units/vela.js
@@ -1,3 +1,3 @@
 UnitTypes.vela.rotateSpeed = 1.8 * 1.15;
-UnitTypes.vela.boostMultiplier = 2.07 * 20;
-UnitTypes.vela.speed = UnitTypes.vela.speed * 2;
+UnitTypes.vela.boostMultiplier = 2.07 * 5;
+UnitTypes.vela.speed = UnitTypes.vela.speed * 4;
