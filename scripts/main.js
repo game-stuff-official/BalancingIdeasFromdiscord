5f473bc2b0ec6c2377001f587b0191cc1cba7ea1
GameStuffOffical <66188005+GameStuffOffical@users.noreply.github.com>
2022-02-19 18:58:22 -0600

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -4,3 +4,12 @@
 
 // Parallax
 Blocks.parallax.requirements = (Items.copper, 1, Items.sillicon, 60;)
+
+// Vela
+UnitTypes.vela.rotateSpeed = 1.8 * 1.15;
+UnitTypes.vela.boostMultiplier = 2.07 * 3;
+UnitTypes.vela.speed = UnitTypes.vela.speed * 4;
+
+// Atrax
+UnitTypes.atrax.legLength = UnitTypes.atrax.legLength * 2;
+UnitTypes.atrax.legCount = UnitTypes.atrax.legCount * 2;
