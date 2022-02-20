fb1468a87293238cbc0daaf4725b992a27937fcb
GameStuffOffical <66188005+GameStuffOffical@users.noreply.github.com>
2022-02-19 19:54:56 -0600

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,16 +3,28 @@
 //require('balancing-mindustry/blocks/atrax');
 
 // Vela
-UnitTypes.vela.rotateSpeed *= 1.15;
+UnitTypes.vela.rotateSpeed *= -1.15;
 UnitTypes.vela.boostMultiplier *= 3;
 UnitTypes.vela.speed *= 4;
 
 // Atrax
 UnitTypes.atrax.legLength *= 4;
 UnitTypes.atrax.legCount *= 2;
-UnitTypes.atrax.speed *= 4;
+UnitTypes.atrax.speed *= 2;
+
+// Gamma
+UnitTypes.gamma.speed *= .5;
+UnitTypes.gamma.rotateSpeed *= .25;
 
 
 // foreshaodow
 Blocks.foreshadow.reloadTime *= 2;
 Blocks.overdriveDome.speedBoost *= 2;
+
+// power sorce
+Blocks.powerSource.laserRange *= 10;
+Blocks.powerSource.maxNodes *= 10;
+
+// mass driver
+Blocks.massDriver.reloadTime *= 2;
+Blocks.massDriver.itemCapaticy *= .5;
