9617dd1417425234955749616c05684b6836e047
GameStuffOffical <66188005+GameStuffOffical@users.noreply.github.com>
2022-02-19 20:13:43 -0600

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@
 //require('balancing-mindustry/blocks/parallax');
 //require('balancing-mindustry/blocks/atrax');
 
-Vars.content.units().filter(unit => !true)
+//Vars.content.units().filter(unit => !true)
 
 // Vela
 UnitTypes.vela.rotateSpeed *= -1.15;
