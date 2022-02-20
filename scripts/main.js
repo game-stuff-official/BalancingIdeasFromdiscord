faf521ece7164c30fdf06198ccaa2cb34da0be63
GameStuffOffical <66188005+GameStuffOffical@users.noreply.github.com>
2022-02-19 18:57:21 -0600

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,5 @@
 //require('balancing-mindustry/blocks/parallax');
 //require('balancing-mindustry/blocks/atrax');
 
+// Parallax
+Blocks.parallax.requirements = (Items.copper, 1, Items.sillicon, 60;)
