3e0d467010c3a5d418a123ece186a62e1c056c42
GameStuffOffical <66188005+GameStuffOffical@users.noreply.github.com>
2022-02-19 18:56:11 -0600

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-require('balancing-mindustry/units/vela');
-require('balancing-mindustry/blocks/parallax');
-require('balancing-mindustry/blocks/silicon-infuser');
+//require('balancing-mindustry/units/vela');
+//require('balancing-mindustry/blocks/parallax');
+//require('balancing-mindustry/blocks/atrax');
 
